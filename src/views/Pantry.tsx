cc10ceb132afd402a530459a190c3bb2d0dd0233
Alex Angell <alexa@Alexs-MBP.lan>
2019-05-17 22:32:54 +1000

    finished db display in recipe book and pantry

--- src/views/Pantry.tsx
+++ src/views/Pantry.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Table, Button } from "antd";
+import Table, { TableProps } from "antd/lib/table";
+import Button from "antd/lib/button";
+import Spin from "antd/lib/spin";
 
-type item = {
+type Item = {
     id: number;
     name: string;
     size: number;
@@ -12,60 +14,82 @@ type item = {
 };
 
 const itemDataURL = "http://localhost:3000/items";
-const columns = [
-    {
-        title: "Name",
-        dataIndex: "name",
-        key: "name"
-    },
-    {
-        title: "Size",
-        dataIndex: "size",
-        key: "size"
-    },
-    {
-        title: "Measure",
-        dataIndex: "measure",
-        key: "measure"
-    },
-    {
-        title: "Stock",
-        dataIndex: "stock",
-        key: "stock"
-    },
-    {
-        title: "Recipes",
-        dataIndex: "recipes",
-        key: "recipes"
-    },
-    {
-        title: "Action",
-        dataIndex: "action",
-        key: "action"
-    }
-];
 
-class Pantry extends React.Component<{}, { tableData: item[] }> {
+interface State {
+    tableData: Item[];
+    loading: boolean;
+}
+
+class Pantry extends React.Component<{}, State> {
+    private columns: TableProps<Item>["columns"];
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            tableData: [],
+            loading: true
+        };
+        this.columns = [
+            {
+                title: "Name",
+                dataIndex: "name",
+                key: "name"
+            },
+            {
+                title: "Size",
+                dataIndex: "size",
+                key: "size"
+            },
+            {
+                title: "Measure",
+                dataIndex: "measure",
+                key: "measure"
+            },
+            {
+                title: "Stock",
+                dataIndex: "stock",
+                key: "stock"
+            },
+            {
+                title: "Recipes",
+                dataIndex: "recipes",
+                key: "recipes",
+                render: (text: string, record: Item) => {
+                    return <Button>{`recipes with ${record.name}`}</Button>;
+                }
+            },
+            {
+                title: "Action",
+                dataIndex: "action",
+                key: "action"
+            }
+        ];
+    }
+
     public async componentDidMount() {
         const itemDataResponse = await fetch(itemDataURL);
         const itemData = await itemDataResponse.json();
-        const tableData = await itemData.map((item: item, indx: number) => {
-            return { ...item, key: indx };
+        const tableData = itemData.map((item: Item) => {
+            return { ...item, key: item.id };
         });
         this.setState({
-            tableData: tableData
+            tableData: tableData,
+            loading: false
         });
     }
 
     public getTableDisplay = () => {
-        if (this.state) {
+        if (!this.state.loading) {
             return (
-                <Table columns={columns} dataSource={this.state.tableData} />
+                <Table
+                    columns={this.columns}
+                    dataSource={this.state.tableData}
+                />
             );
         }
 
-        return <div>Data loading</div>;
+        return <Spin size="large" />;
     };
+
     render() {
         return (
             <React.Fragment>
