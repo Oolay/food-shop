409c5539b1ebe4723e5bad60e400eb1af0c1119a
Alex Angell <alexa@Alexs-MacBook-Pro.local>
2019-05-18 17:46:16 +1000

    added logo with css to navNar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,44 +1,62 @@
 import React from "react";
 import Layout from "antd/lib/layout";
 import Menu from "antd/lib/menu";
+import withStyles from "react-jss";
 import "antd/dist/antd.css";
 
+import foodShopLogo from "../foodShopLogo2.png";
 const { Header } = Layout;
 
+const styles = {
+    logo: {
+        height: "50px",
+        width: "50px",
+        float: "left",
+        margin: "8px",
+        marginRight: "36px"
+    }
+};
+
 class NavBar extends React.Component<{
+    classes: any;
     handleMenuClick(view: "shoppingList" | "recipeBook" | "pantry"): () => void;
 }> {
     render() {
+        const { classes } = this.props;
+
         return (
-            <Header>
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={["shoppingList"]}
-                    style={{ lineHeight: "64px" }}
-                >
-                    <Menu.Item
-                        key="shoppingList"
-                        onClick={this.props.handleMenuClick("shoppingList")}
+            <Layout>
+                <Header>
+                    <img className={classes.logo} src={foodShopLogo} />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={["shoppingList"]}
+                        style={{ lineHeight: "64px" }}
                     >
-                        Shopping lists
-                    </Menu.Item>
-                    <Menu.Item
-                        key="recipeBook"
-                        onClick={this.props.handleMenuClick("recipeBook")}
-                    >
-                        Recipe book
-                    </Menu.Item>
-                    <Menu.Item
-                        key="pantry"
-                        onClick={this.props.handleMenuClick("pantry")}
-                    >
-                        Pantry
-                    </Menu.Item>
-                </Menu>
-            </Header>
+                        <Menu.Item
+                            key="shoppingList"
+                            onClick={this.props.handleMenuClick("shoppingList")}
+                        >
+                            Shopping lists
+                        </Menu.Item>
+                        <Menu.Item
+                            key="recipeBook"
+                            onClick={this.props.handleMenuClick("recipeBook")}
+                        >
+                            Recipe book
+                        </Menu.Item>
+                        <Menu.Item
+                            key="pantry"
+                            onClick={this.props.handleMenuClick("pantry")}
+                        >
+                            Pantry
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+            </Layout>
         );
     }
 }
 
-export default NavBar;
+export default withStyles(styles)(NavBar);
